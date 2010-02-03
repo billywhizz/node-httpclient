8f55b9bab61bbaf3836296bb5ce67b3021bf4d84
Andrew Johnston <apjohnsto@gmail.com>
2010-02-03 12:34:30 +0000

    initial commit

---
+++ example.js
@@ -0,0 +1,33 @@
+var sys = require("sys");
+var httpcli = require("./httpclient");
+
+var url = "http://www.betfair.com";
+var surl = "https://www.betfair.com";
+
+function verifyTLS(request) {
+	sys.puts(sys.inspect(request));
+	return true;
+}
+
+var client = new httpcli.httpclient();	
+
+// a simple http request with default options (gzip off, keepalive off, https off)
+client.perform(url, "GET", function(result) {
+	sys.puts(sys.inspect(result));
+}, null);
+
+var client2 = new httpcli.httpclient();
+
+// nested calls with gzip compression and keep-alive
+client2.perform(url, "GET", function(result) {
+	sys.puts(sys.inspect(result));
+	client2.perform(url, "GET", function(result) {
+		sys.puts(sys.inspect(result));
+		// https request with callback handling of certificate validation
+		client2.perform(surl, "GET", function(result) {
+			sys.puts(sys.inspect(result));
+		}, null, {"Accept-Encoding" : "none,gzip", "Connection" : "close"}, verifyTLS);
+	}, null, {"Accept-Encoding" : "none,gzip", "Connection" : "Keep-Alive"});
+}, null, {"Accept-Encoding" : "none,gzip", "Connection" : "Keep-Alive"});
+
+
---
+++ tests.js
@@ -0,0 +1,61 @@
+var sys = require("sys");
+var httpcli = require("./httpclient");
+var testnum = 0;
+var client = new httpcli.httpclient();	
+
+// return false to reject the certificate
+function verifyTLS(request) {
+/*
+	sys.puts(sys.inspect(request));
+*/
+	return true;
+}
+
+// reuses one http client for all requests
+function runtest(url, method, data, exheaders, tlscb) {
+	var mytestnum = ++testnum;
+	sys.puts(mytestnum + " : " + url);
+	var startTime = new Date();
+	client.perform(url, method, function(result) {
+		var tt = new Date().getTime() - startTime.getTime();
+		sys.puts(mytestnum + " : " + result.response.status + " : " + tt);
+/*
+		sys.puts(sys.inspect(result));
+*/
+	}, data, exheaders, tlscb);
+}
+
+// creates a new client for each request
+function runtest2(url, method, data, exheaders, tlscb) {
+	var mytestnum = ++testnum;
+	sys.puts(mytestnum + " : " + url);
+	var startTime = new Date();
+	var client = new httpcli.httpclient();	
+	client.perform(url, method, function(result) {
+		var tt = new Date().getTime() - startTime.getTime();
+		sys.puts(mytestnum + " : " + result.response.status + " : " + tt);
+/*
+		sys.puts(sys.inspect(result));
+*/
+	}, data, exheaders, tlscb);
+}
+
+function runtests(url, foo) {
+	foo("http://" + url, "GET", null, {"Connection" : "close"}, null);
+	foo("https://" + url, "GET", null, {"Connection" : "close"}, verifyTLS);
+	foo("https://" + url, "GET", null, {"Connection" : "close"});
+	foo("http://" + url, "GET", null, {"Accept-Encoding" : "gzip", "Connection" : "close"}, null);
+	foo("https://" + url, "GET", null, {"Accept-Encoding" : "gzip", "Connection" : "close"});
+	foo("https://" + url, "GET", null, {"Accept-Encoding" : "gzip", "Connection" : "close"}, verifyTLS);
+	foo("http://" + url, "GET", null, {"Connection" : "Keep-Alive"}, null);
+	foo("https://" + url, "GET", null, {"Connection" : "Keep-Alive"});
+	foo("https://" + url, "GET", null, {"Connection" : "Keep-Alive"}, verifyTLS);
+	foo("http://" + url, "GET", null, {"Accept-Encoding" : "gzip", "Connection" : "Keep-Alive"}, null);
+	foo("https://" + url, "GET", null, {"Accept-Encoding" : "gzip", "Connection" : "Keep-Alive"});
+	foo("https://" + url, "GET", null, {"Accept-Encoding" : "gzip", "Connection" : "Keep-Alive"}, verifyTLS);
+}
+
+// put a domain name and path here for an address that has the features you want to test (ssl, gzip, keepalive)
+// be sure to exclude the protocol
+runtests("www.google.co.uk", runtest);
+runtests("www.google.co.uk", runtest2);
